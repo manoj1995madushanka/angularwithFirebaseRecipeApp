a05930b9ffd1533ff8c8b6a6fc9555d160c66975
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-02-06 21:07:45 +0530

    complete

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,14 +1,35 @@
-import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
-import {RecipeService} from '../recipes/recipe.service';
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/Rx';
+
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
-  private constructor(private http: Http, private recipeService: RecipeService) {
-
-  }
+  constructor(private http: Http, private recipeService: RecipeService) {}
 
   storeRecipes() {
-    return this.http.put('https://angular-recipe-book-b506e.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    return this.http.put('https://ng-recipe-book.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    this.http.get('https://ng-recipe-book.firebaseio.com/recipes.json')
+      .map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 }
